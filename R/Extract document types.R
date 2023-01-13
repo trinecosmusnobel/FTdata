9ba0b3103d768c20a3901806082a6522e8e0717f
trinecosmusnobel <nobel@ruc.dk>
2023-01-13 17:14:54 +0100

    removed empty export function

--- R/Extract document types.R
+++ R/Extract document types.R
@@ -3,5 +3,3 @@ json <- jsonlite::fromJSON(rawToChar(response$content))
 FT_Types <- as.data.frame(json) %>% dplyr::as_tibble() %>% dplyr::rename("Type" = value.name,
                                                            "URL.prefix" = value.url) %>%
   dplyr::select(!odata.metadata)
-
-usethis::use_r()
