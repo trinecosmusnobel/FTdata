e1cb83c70866b20eb5db147da12203bc9cc7a06d
trinecosmusnobel <nobel@ruc.dk>
2023-01-13 16:44:44 +0100

    oops

--- R/Extract document types.R
+++ R/Extract document types.R
@@ -2,6 +2,6 @@ response <- httr::GET("https://oda.ft.dk/API/")
 json <- jsonlite::fromJSON(rawToChar(response$content))
 FT_Types <- as.data.frame(json) %>% as_tibble() %>% rename("Type" = value.name,
                                                            "URL.prefix" = value.url) %>%
-  dplyr::selectselect(!odata.metadata)
+  dplyr::select(!odata.metadata)
 
 usethis::use_r()
