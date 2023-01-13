1805c426ea2041eb21120f0235a78bba10a1dbb3
trinecosmusnobel <nobel@ruc.dk>
2023-01-13 15:52:19 +0100

    add packages

--- R/Possible metadata criteria.R
+++ R/Possible metadata criteria.R
@@ -1,19 +1,19 @@
 #Possible search criteria for each type of document search
 
-response <- GET("https://oda.ft.dk/API/")
+response <- httr::GET("https://oda.ft.dk/API/")
 json <- fromJSON(rawToChar(response$content))
 FT_Types <- as.data.frame(json) %>% as_tibble() %>% rename("Type" = value.name,
-                                                           "URL.prefix" = value.url) %>% 
+                                                           "URL.prefix" = value.url) %>%
   select(!odata.metadata)
 
 
 possible.meta <- list()
 
 for (i in seq(FT_Types$URL.prefix)) {
-  
+
   response <- GET(paste0("https://oda.ft.dk/API/", (FT_Types$URL.prefix[i])))
   df <- fromJSON(rawToChar(response$content))$value %>% as_tibble()
-  
+
   possible.meta[[i]] <- colnames(df)
 
   }
